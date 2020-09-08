28f35a60f49f8a555bd088264166481506466ad4
roseheart13 <70570030+roseheart13@users.noreply.github.com>
2020-09-08 15:43:37 +0200

    Add project files.

---
+++ Print20/Print20.cpp
@@ -0,0 +1,43 @@
+
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << "i = " << i << " ";
+            std::cout << "j = " << j << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    
+    //int i = 0;
+
+    //for (int i = 0; i <= 20; i++) { //i++   i=i+1
+    //    if (i % 2 == 0) {
+    //        std::cout << " " << i;
+    //    }
+    //}
+
+    //std::cout << std::endl;
+
+    //do {
+    //    if (i % 2 == 0) {
+    //        std::cout << " " << i;
+    //    }
+    //    i++;
+
+    //} while(i <= 20);
+    //std::cout << std::endl;
+
+    //std::cout << std::endl;
+
+    
+
+    return 0;
+}
+
